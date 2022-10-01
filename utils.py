331270980860bae74de4a784208d2bde69a037f4
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2022-10-01 09:35:54 -0500

    Initial commit :rocket:

---
+++ utils.py
@@ -0,0 +1,15 @@
+def invert_text(text):
+    """Inverts text.
+
+    Parameters
+    ----------
+    text : str
+        Text to invert.
+
+    Returns
+    -------
+    str
+        Inverted text.
+    """
+
+    return text[::-1]
